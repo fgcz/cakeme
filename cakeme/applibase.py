891e0d1f42055f8b2dd48d3bd3a42b38a52a9f9d
bfabric <bfabric@fgcz-net.unizh.ch>
2015-11-02 09:09:27 +0100

    adding scp copy class

--- cakeme/applibase.py
+++ cakeme/applibase.py
@@ -3,11 +3,13 @@ import logging
 import shlex
 import os
 import subprocess
+import fileutils
 
 ProcessValues = namedtuple("ProcessValues", "return_code out err")
 
 def setUpLogging(destination='/var/tmp/myapp.log'):
     logger = logging.getLogger('myapp')
+    fileutils.make_path_to_file(destination)
     hdlr = logging.FileHandler(destination)
     formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
     hdlr.setFormatter(formatter)
@@ -36,8 +38,49 @@ class ApplicationBase:
             self.logger.error("test {}".format(str(e)))
 
     def logResults(self):
-        self.logger.info("{} : {}".format(self.APP_NAME,self.RESULT.out))
-        #self.logger.error("{} : {}".format(self.APP_NAME, self.RESULT.err))
-
+        self.logger.info("{appname} : ret {returnval} out {out}  err {err}".format(appname = self.APP_NAME,
+                                                                    returnval=self.RESULT.return_code,
+                                                                    out = self.RESULT.out,
+                                                                    err= self.RESULT.err))
     def run(self):
         pass
+
+
+class SCPCopy(ApplicationBase):
+    def __init__(self,result_dir):
+        ApplicationBase.__init__(self, "SCPCopy", result_dir)
+
+    def stageFileTo(self, user, destination_computer, fromfile , tofile):
+        if not os.path.exists(fromfile):
+            self.logger.error("no such file : {}".format(fromfile))
+        fromfile.strip()
+        scp_command = "scp {file} {user}@{machine}:{tofile} ".format(file=fromfile, user=user,
+                                                                     machine=destination_computer,
+                                                                     tofile=tofile)
+        self.executeCommand(scp_command)
+        self.logResults()
+        return self.RESULT.return_code
+
+    def getFileFrom(self, user, source_computer, fromfile,  tofile):
+        if not os.path.exists(file):
+            self.logger.error("no such file : {}".format(file))
+        fromfile.strip()
+        scp_command = "scp {user}@{machine}:{fromfile} {tofile} ".format(user=user, machine=source_computer,
+                                                                         fromfile=fromfile, tofile=tofile)
+        self.executeCommand(scp_command)
+        self.logResults()
+        return self.RESULT.return_code
+
+
+#class SpecLApplication(ApplicationBase):
+if __name__ == "__main__":
+    '''
+    datfile = sys.argv[1]
+    res = get_db_from_mascot_dat(datfile)
+    print res
+    res = cakeme.fileutils.find_file(res, "/usr/local/mascot/sequence/")
+    print res
+    '''
+    scp = SCPCopy("testlog.log")
+    ret= scp.stageFileTo("bfabric", "fgcz-s-021", "setup.py" ,"/srv/www/htdocs//p1000/bfabric/Proteomics/DIA_Assay_Library_Generator/2015/2015-10/2015-10-27//workunit_136124/206686.zip")
+    print ret
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name='cakeme',
-	version='0.0.3',
+	version='0.0.4',
 	author='Witold Wolski',
 	author_email='wolski@gmail.com',
 	description='cook up a call to a third party application',
